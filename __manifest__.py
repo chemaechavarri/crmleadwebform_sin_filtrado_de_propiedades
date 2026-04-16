dbfbb7d3ea34533be4b739bdc55e8c9948ed751e
chemaechavarri <chemaechavarri@gmail.com>
2026-04-15 18:25:38 -0600

    Create __manifest__.py

---
+++ __manifest__.py
@@ -0,0 +1,15 @@
+{
+    'name': 'CRM Lead Web Form',
+    'version': '18.0.1.0.0',
+    'depends': [
+        'website',
+        'crm',
+    ],
+    'data': [
+        'views/website_lead_form.xml',
+        'views/website_thank_you.xml',
+    ],
+    'installable': True,
+    'application': True,
+    'license': 'LGPL-3',
+}
